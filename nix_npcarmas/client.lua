154d502a2d6fe76f372a7185c8957f4232d77f98
Nix <93496101+NixCD@users.noreply.github.com>
2022-03-04 21:58:47 +0100

    Delete nix_npcarmas directory

--- nix_npcarmas/client.lua
+++
@@ -1,8 +0,0 @@
-Citizen.CreateThread(function()
-    while true do
-      Citizen.Wait(1)
-      RemoveAllPickupsOfType(0xF9AFB48F)
-      RemoveAllPickupsOfType(0xA9355DCD)
-      RemoveAllPickupsOfType(0xDF711959)
-    end
-  end)
